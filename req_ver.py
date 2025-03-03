8264ec1f0bd6dded2138db488aba4ae1208f1f9b
manimaranbtech <86833925+manimaranbtech@users.noreply.github.com>
2025-03-03 13:27:56 +0530

    Update req_ver.py

--- req_ver.py
+++ req_ver.py
@@ -1,3 +1,15 @@
+# Check pdf2image version
+import pdf2image
+print("pdf2image version:", pdf2image.__version__)
+
 # Check Pillow version
 from PIL import Image
 print("Pillow version:", Image.__version__)
+
+# Check if poppler-utils is installed (optional)
+import subprocess
+try:
+    subprocess.run(["pdftoppm", "-v"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print("poppler-utils is installed!")
+except FileNotFoundError:
+    print("poppler-utils is NOT installed.")
