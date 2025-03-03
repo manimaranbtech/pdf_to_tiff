e116ac5328aa8702e892a3ba221aead749a57d89
manimaranbtech <86833925+manimaranbtech@users.noreply.github.com>
2025-03-03 09:37:46 +0530

    Update req_ver.py

--- req_ver.py
+++ req_ver.py
@@ -1,11 +1,3 @@
-# Check numpy version
-import numpy
-print(numpy.__version__)
-
 # Check Pillow version
 from PIL import Image
 print("Pillow version:", Image.__version__)
-
-# Check pdf2image version
-import pdf2image
-print("pdf2image version:", pdf2image.__version__)
