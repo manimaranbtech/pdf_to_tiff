f25863284b0a81e5c9d15fea2af8b214077bdbac
manimaranbtech <86833925+manimaranbtech@users.noreply.github.com>
2025-03-04 14:37:17 +0530

    Update pdf_tiff.py

--- pdf_tiff.py
+++ pdf_tiff.py
@@ -1,13 +1,15 @@
 from pdf2image import convert_from_path
-from PIL import Image
+from PIL import Image, ImageOps
 
 # Path to the PDF file
-pdf_path = "example.pdf"
+input_path = "example.pdf"
 
 # Convert PDF to a list of PIL images
-images = convert_from_path(pdf_path)
+images = convert_from_path(input_path, dpi=300)
+bw_image = images[0].convert('1')
+inverted_image = ImageOps.invert(bw_image.convert('L')).convert('1')
 
 # Save all pages as a multi-page TIFF
 tiff_path = "output.tiff"
-images[0].save(tiff_path, save_all=True, append_images=images[1:], compression="tiff_deflate")
+inverted_image.save(tiff_path, format='TIFF', dpi=(300, 300), compression='tiff_ccitt')
 print(f"Saved multi-page TIFF as {tiff_path}")
